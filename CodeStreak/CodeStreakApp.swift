a2d25716e7403431f25de977a282038d62af647d
FrancoAquino23 <francoaquialla@gmail.com>
2025-10-28 18:42:26 -0600

    Initial Commit

---
+++ CodeStreak/CodeStreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeStreakApp.swift
+//  CodeStreak
+//
+//  Created by Fran on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeStreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
