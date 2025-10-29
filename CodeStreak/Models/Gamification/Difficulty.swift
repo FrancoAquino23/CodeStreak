91df92b0bdb11d81c80363e6c2f222b9175cf3c4
FrancoAquino23 <francoaquialla@gmail.com>
2025-10-28 21:13:35 -0600

    Enums (Gaming Tool) defined.

---
+++ CodeStreak/Models/Gamification/Difficulty.swift
@@ -0,0 +1,25 @@
+
+import Foundation
+
+enum Difficulty: Int, Codable, CaseIterable {
+    case easy = 1
+    case medium = 2
+    case hard = 3
+    case expert = 4
+    case master = 5
+    
+    var xpMultiplier: Double {
+        switch self {
+        case .easy:
+            return 1.0
+        case .medium:
+            return 1.5
+        case .hard:
+            return 2.0
+        case .expert:
+            return 2.5
+        case .master:
+            return 3.0
+        }
+    }
+}
---
+++ CodeStreak/Models/Gamification/Technology.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+
+enum Technology: String, Codable, CaseIterable {
+    case swift = "Swift"
+    case java = "Java"
+    case python = "Python"
+    case javascript = "JavaScript"
+    case go = "Go"
+}
