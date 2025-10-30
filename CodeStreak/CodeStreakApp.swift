95edbe177cce09e5ba34539a59292fca0c91aacb
FrancoAquino23 <francoaquialla@gmail.com>
2025-10-30 08:38:36 -0600

    Main app integrated.

--- CodeStreak/CodeStreakApp.swift
+++ CodeStreak/CodeStreakApp.swift
@@ -1,17 +1,48 @@
-//
-//  CodeStreakApp.swift
-//  CodeStreak
-//
-//  Created by Fran on 28/10/25.
-//
 
 import SwiftUI
 
 @main
 struct CodeStreakApp: App {
+    let dataController = DataController()
+    var persistenceManager: PersistenceManager {
+        return PersistenceManager(context: dataController.getContext())
+    }
+    var rewardService: RewardGranting {
+        return RewardService(dataManager: persistenceManager)
+    }
+    var streakService: StreakCalculator{
+        return StreakService(dataManager: persistenceManager, rewardService: rewardService)
+    }
+    var homeViewModel: HomeViewModel {
+        return HomeViewModel(streakService: streakService, dataManager: persistenceManager)
+    }
+    var profileViewModel: ProfileViewModel {
+        return ProfileViewModel(dataManager: persistenceManager)
+    }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
+                .environment(homeViewModel)
+                .environment(profileViewModel)
+        }
+    }
+}
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+            StoreView()
+                .tabItem {
+                    Label("Store", systemImage: "cart.fill")
+                }
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.fill")
+                }
         }
     }
 }
